ca1bb1f1570b1b97c3ae82352089039d86c9c469
natter1 <njoehrmann@gmail.com>
2020-08-12 22:22:34 +0200

    freez version 0.0.11

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (root_path / "README.rst").read_text()
 
 setup(
     name='python-pptx-interface',
-    version='0.0.10',
+    version='0.0.11',
     packages=['pptx_tools', 'pptx_tools.examples'],
     url='https://github.com/natter1/python_pptx_interface.git',
     license='MIT',
